1e83da56d05a71fc844ca903506a5fab7c96d207
Hiep Tran Quang <hiep.tranquang@koro.vn>
2018-06-08 15:12:15 +0700

    wew angularfire2 can do it

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: Observable<any>;
+  constructor(db: AngularFireDatabase) {
+    this.title = db.object('.info/connected').valueChanges();
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD6E9OYBX72KCRcnwPAzDjVy8MmDbbEgfw",
+    authDomain: "stackblitzfire.firebaseapp.com",
+    databaseURL: "https://stackblitzfire.firebaseio.com",
+    storageBucket: "stackblitzfire.appspot.com",
+    messagingSenderId: "42917465053"
+  }
+};
